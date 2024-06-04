972cfdc4f68ce72546edf5ddda8a06dcec46f60a
정호연 <jea7730@gmail.com>
2024-06-04 18:07:57 +0900

    data 배열 확인

--- a.js
+++ a.js
@@ -2,4 +2,10 @@ const fs = require('fs');
 
 // todo-1 csv 파일 읽기
 const data = fs.readFileSync('utf8.csv', 'utf-8');
-console.log(data);
+// console.log(data);
+
+//todo-2 data 배열로 변환
+const array = [];
+
+array.push(data);
+console.log(array);
