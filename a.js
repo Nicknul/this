54d778ef8e2c09b5c7d2852d28a2ab2107014835
정호연 <jea7730@gmail.com>
2024-06-04 14:46:54 +0900

    csv 파일 읽기 성공

--- a.js
+++ a.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
 const csvPath = '한국전력공사_지역별 전기차 충전기 현황정보_20221231.csv';
-
+// todo-1 csv 데이터를 담을 빈 배열 생성
 let a = [];
+// todo-2 csv 파일 읽기
+let b = fs.readFileSync(csvPath, 'utf-8');
+console.log(b);
