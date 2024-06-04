660565dc9101542f64a578cd139ccf99846a942b
정호연 <jea7730@gmail.com>
2024-06-04 16:23:42 +0900

    지역 추출 성공

--- test.js
+++ test.js
@@ -4,11 +4,20 @@ const fs = require('fs');
 const data = fs.readFileSync('utf8.csv', 'utf-8');
 // console.log(data);
 
-//todo-2 split
+// todo-2 split으로 문자열 뒤 공백 없애기
 let zero = data.split('\r\n');
-console.log(zero[1]);
+// console.log(zero);
+let city = zero[1].split(',');
+// console.log(city);
 
-zero.forEach((index, value) => {
-  let city = index.split(',');
-  console.log(city);
-});
+//* zero에 인덱스 번호를 입력해야 거기에 해당되는 자료가 나타남
+// let city = zero[0].split(',');
+// console.log(city);
+
+// todo-3 forEach로 문자열 모든 값 도출하기
+for (let element in zero) {
+  let a = zero[element];
+  let b = a.split(',');
+  let c = b[0];
+  console.log(c);
+}
