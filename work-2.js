01b935eb99244d2563342290b9afe5cd53d1a0b9
정호연 <jea7730@gmail.com>
2024-06-04 10:51:56 +0900

    setter, 에러 생성

--- work-2.js
+++ work-2.js
@@ -3,7 +3,14 @@ class WorkTwo {
     this.a = a;
     this.b = b;
   }
+  set a(value) {
+    if (typeof value === 'number') {
+      return (this._a = value);
+    } else {
+      throw new Error('a는 숫자만 입력만 가능합니다.');
+    }
+  }
 }
 
-let test = new WorkTwo(1, 2);
+let test = new WorkTwo('정호연', 2);
 console.log(test);
