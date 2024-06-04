e0813d7351bf9de4ba758a73a9f818a374201bf9
정호연 <jea7730@gmail.com>
2024-06-04 15:56:01 +0900

    split 진행 중

--- test.js
+++ test.js
@@ -3,3 +3,12 @@ const fs = require('fs');
 // todo-1 csv 파일 읽기
 const data = fs.readFileSync('utf8.csv', 'utf-8');
 // console.log(data);
+
+//todo-2 split
+let zero = data.split('\r\n');
+console.log(zero[1]);
+
+zero.forEach((index, value) => {
+  let city = index.split(',');
+  console.log(city);
+});
