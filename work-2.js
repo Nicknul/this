7b8491a5a4053899717d3f8238ddfaa80ecc0ed1
정호연 <jea7730@gmail.com>
2024-06-04 10:56:29 +0900

    덧셈 밖에서 해보기

--- work-2.js
+++ work-2.js
@@ -10,7 +10,16 @@ class WorkTwo {
       throw new Error('a는 숫자만 입력만 가능합니다.');
     }
   }
+  set b(value) {
+    if (typeof value === 'number') {
+      return (this._b = value);
+    } else {
+      throw new Error('b는 숫자만 입력만 가능합니다.');
+    }
+  }
 }
 
-let test = new WorkTwo('정호연', 2);
+let test = new WorkTwo(1, 2);
 console.log(test);
+
+console.log(test._a + test._b);
