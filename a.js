5364c7e77cd38a386dd3d7865981d99283c91713
정호연 <jea7730@gmail.com>
2024-06-05 12:47:43 +0900

    빈 객체 안에 추출한 data로 key와 value로 넣어보자? git commit -m

--- a.js
+++ a.js
@@ -15,13 +15,24 @@ const splitData = data.split('\r\n');
 // console.log(splitData);
 
 // todo-4 split한 data 배열의 값을 하나씩 나오게 하자
-// const city = splitData[0].split(',');
+const key = splitData[0].split(',');
 // console.log(city);
-for (let i = 0; i < splitData.length; i++) {
-  // console.log(splitData[i]);
-}
+
+// for (let i = 0; i < splitData.length; i++) {
+// console.log(splitData[i]);
+// }
 
 // todo-5 배열 값 안에 있는 또 다른 값을 가져오자
-for (let i = 0; i < splitData.length; i++) {
-  console.log(splitData[i].split(','));
+// for (let i = 0; i < splitData.length; i++) {
+// console.log(splitData[i].split(','));
+// }
+
+// todo-6 빈 객체 안에 추출한 data로 key와 value로 넣어보자
+for (let i = 1; i < splitData.length; i++) {
+  const value = splitData[i].split(',');
+  const obj = {};
+  for (let j = 0; j < key.length; j++) {
+    obj[key[j]] = value[j];
+  }
+  console.log(obj);
 }
