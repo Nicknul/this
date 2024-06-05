e0cc00b4d60fb18d51052ec4ce390d8e94b8554c
정호연 <jea7730@gmail.com>
2024-06-05 09:45:45 +0900

    split를 이용하여 불 필요한 문자 지우기

--- a.js
+++ a.js
@@ -5,7 +5,11 @@ const data = fs.readFileSync('utf8.csv', 'utf-8');
 // console.log(data);
 
 //todo-2 data 배열로 변환
-const array = [];
+// const array = [];
 
-array.push(data);
-console.log(array);
+// array.push(data);
+// console.log(array);
+
+// todo-3 split를 이용하여 불 필요한 문자 지우기
+const splitData = data.split('\r\n');
+console.log(splitData);
